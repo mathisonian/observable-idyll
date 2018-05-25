6ff7a30cc0a7922520cd29cde3282cd826bd96e8
Matthew Conlen <mc@mathisonian.com>
2018-05-25 12:17:48 -0700

    initial tests

--- components/observable.js
+++ components/observable.js
@@ -3,30 +3,10 @@ const {Runtime, Inspector, Library } = require("../lib/notebook-runtime");
 import notebook from "./notebooks/d3-change-line-chart";
 
 
-
-const cleanImports = (nb) => {
-  return nb;
-  nb.modules = nb.modules.map((mod) => {
-    mod.variables = mod.variables.map((v) => {
-      if (v.name && v.name === 'd3') {
-        v.value = function() { return d3 }
-      }
-      return v;
-    })
-    return mod;
-  })
-  return nb;
-}
-
-
-
-
 class CustomComponent extends React.Component {
 
-
   componentDidMount() {
-    console.log(cleanImports(notebook));
-    const { runtime, main } = Runtime.load(cleanImports(notebook), Object.assign({}, new Library(), { width: 600 }), (variable) => {
+    const { runtime, main } = Runtime.load(notebook, Object.assign({}, new Library(), { width: 600 }), (variable) => {
 
       if (variable.name && Object.keys(this.props.variables || {}).indexOf(variable.name) > -1) {
         variable.value = (function* (){ while(true) yield this.props.variables[variable.name] }).bind(this);
